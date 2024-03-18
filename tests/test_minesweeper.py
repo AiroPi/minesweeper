bda421b68645a127a576f03ce8ee3ee2cba7ef16
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2024-03-18 21:21:05 +0100

    initial commit

---
+++ tests/test_minesweeper.py
@@ -0,0 +1,12 @@
+# There is no tests atm, but the file exist to assert the code can be executed in older python versions (because ruff doesn't interpret the code, it doesn't detect retro-compatibility.)
+
+import minesweeper
+import pytest
+
+
+def test_nothing():
+    pass
+
+
+del minesweeper
+del pytest
